bc8c1d5c9b98914564fbd97b9a427842b69abe5b
Adilxa <adilhanimerov82@gmail.com>
2025-08-23 17:23:23 +0600

    svg

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config: any
-  ) => {
+  // Disable image optimization completely
+  images: {
+    unoptimized: true,
+    loader: 'custom',
+    loaderFile: './image-loader.js'
+  },
+  // Handle SVGs
+  webpack: (config: any) => {
+    // Handle SVGs as React components
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
+
+    // Handle regular images
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/images/',
+            outputPath: 'static/images/',
+          },
+        },
+      ],
+    });
+
     return config;
   },
 };
